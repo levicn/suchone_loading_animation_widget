24ee0d7fdd3cf0dbb947ed61fa015469008e2369
levicn <liwei1987cn@163.com>
2022-09-20 22:24:39 +0800

    保留三个

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:suchone_loading_animation_widget/loading_animation_widget.dart';
 
 const Color _kAppColor = Color(0xFFFDDE6F);
 const double _kSize = 100;
@@ -158,83 +158,12 @@ final listOfAnimations = <AppBody>[
     ),
   ),
   AppBody(
-    'waveDots',
-    LoadingAnimationWidget.waveDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'inkDrop',
-    LoadingAnimationWidget.inkDrop(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'twistingDots',
-    LoadingAnimationWidget.twistingDots(
-      leftDotColor: const Color(0xFF1A1A3F),
-      rightDotColor: const Color(0xFFEA3799),
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'threeRotatingDots',
-    LoadingAnimationWidget.threeRotatingDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'staggeredDotsWave',
-    LoadingAnimationWidget.staggeredDotsWave(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'fourRotatingDots',
-    LoadingAnimationWidget.fourRotatingDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'fallingDot',
-    LoadingAnimationWidget.fallingDot(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'prograssiveDots',
-    LoadingAnimationWidget.prograssiveDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
     'discreteCircle',
     LoadingAnimationWidget.discreteCircle(
         color: Colors.white,
         size: _kSize,
         secondRingColor: Colors.black,
         thirdRingColor: Colors.purple),
-  ),
-  AppBody(
-    'threeArchedCircle',
-    LoadingAnimationWidget.threeArchedCircle(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'bouncingBall',
-    LoadingAnimationWidget.bouncingBall(
-      color: Colors.white,
-      size: _kSize,
-    ),
   ),
   AppBody(
     'flickr',
@@ -245,59 +174,10 @@ final listOfAnimations = <AppBody>[
     ),
   ),
   AppBody(
-    'hexagonDots',
-    LoadingAnimationWidget.hexagonDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'beat',
-    LoadingAnimationWidget.beat(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'twoRotatingArc',
-    LoadingAnimationWidget.twoRotatingArc(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'horizontalRotatingDots',
-    LoadingAnimationWidget.horizontalRotatingDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
     'newtonCradle',
     LoadingAnimationWidget.newtonCradle(
       color: Colors.white,
       size: 2 * _kSize,
-    ),
-  ),
-  AppBody(
-    'stretchedDots',
-    LoadingAnimationWidget.stretchedDots(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'halfTriangleDot',
-    LoadingAnimationWidget.halfTriangleDot(
-      color: Colors.white,
-      size: _kSize,
-    ),
-  ),
-  AppBody(
-    'dotsTriangle',
-    LoadingAnimationWidget.dotsTriangle(
-      color: Colors.white,
-      size: _kSize,
     ),
   ),
 ];
--- lib/loading_animation_widget.dart
+++ lib/loading_animation_widget.dart
@@ -1,3 +1,3 @@
-library loading_animation_widget;
+library suchone_loading_animation_widget;
 
 export 'src/build_loading_animation.dart';
